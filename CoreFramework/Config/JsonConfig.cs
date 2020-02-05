6629b343a7526217569fa55f471e44bcd5f62ba6
Ngwesse Elvis <ngwesseelvis@Ngwesses-MBP.lan>
2020-02-05 18:36:47 +0000

    second commit

---
+++ CoreFramework/Config/JsonConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreFramework.Config
+{
+    public static class JsonConfig
+    {
+        private static IConfiguration _configuration { get; set; }
+
+       public static string GetJsonValue(string key)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appConfig.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("hubConfig.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("nodeConfig.json", optional: false, reloadOnChange: true);
+
+            _configuration = builder.Build();
+
+            return _configuration[key];
+        }
+    }
+}
